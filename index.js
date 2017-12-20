2d374df761ab3b11a60f16688f97d201201b0716
nancy sin <nsin@sbcglobal.net>
2017-12-20 03:21:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ class BoardMember{
 }
 
  veto (){
-   return 'No, I must disagree'
+   return "No, I must disagree'"
  }
  approve() {
-   return 'You can do that!'
+   return "You can do that!"
  }
  doCharity(){
-   return 'I like to help people.'
+   return "I like to help people."
  }
  releasePressStatement(){
-   return 'You will see great things from Scuber'
+   return "You will see great things from Scuber"
  }
  sayHi(){
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
@@ -24,7 +24,7 @@ class BoardMember{
 
 
  class Ceo extends BoardMember{
-   hireEmployee(){ 
-     return 'Welcome aboard!'}
+   hireEmployee(){
+     return "Welcome aboard!"}
  }
 }
